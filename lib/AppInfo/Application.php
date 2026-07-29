09c607035440837261b2d2d6884cc8377d186e45
Metrat <disparam@gmail.com>
2026-07-29 22:57:08 +0300

    Fix: Simplify app structure for PHP 8.4 compatibility
    - Remove Activity/Provider/Filter/Setting registrations (classes not yet implemented)
    - Remove BackgroundJob registrations from info.xml (classes not yet implemented)
    - Simplify LoggerFactory and AppLogger (remove external dependencies)
    - Add TestCommand for verifying OCC command registration
    - Update Application.php to use simplified service registration
    - Fix PHP 8.4 compatibility issues
    
    This is a minimal working version. Full features will be added in subsequent releases.

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -2,27 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * @copyright Copyright (c) 2024 Metrat <disparam@gmail.com>
- *
- * @author Metrat <disparam@gmail.com>
- *
- * @license AGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace OCA\AdminOffboard\AppInfo;
 
 use OCA\AdminOffboard\Adapter\DeviceAdapter;
@@ -37,23 +16,21 @@ use OCA\AdminOffboard\Db\Mapper\JobMapper;
 use OCA\AdminOffboard\Db\Repository\AuditLogRepository;
 use OCA\AdminOffboard\Db\Repository\DeviceRepository;
 use OCA\AdminOffboard\Db\Repository\JobRepository;
+use OCA\AdminOffboard\Driver\DriverFactory;
 use OCA\AdminOffboard\Logger\LoggerFactory;
 use OCA\AdminOffboard\Queue\JobProcessor;
 use OCA\AdminOffboard\Queue\JobQueue;
+use OCA\AdminOffboard\Queue\QueueManager;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Bootstrap\IBootContext;
-use OCP\DB\Connection;
 use OCP\IConfig;
 use OCP\IDBConnection;
 use OCP\IUserManager;
 use OCP\IUserSession;
 use OCP\Security\ISecureRandom;
 
-/**
- * Main application class
- */
 class Application extends App implements IBootstrap
 {
     public const APP_ID = 'adminoffboard';
@@ -63,12 +40,9 @@ class Application extends App implements IBootstrap
         parent::__construct(self::APP_ID);
     }
 
-    /**
-     * Register all services
-     */
     public function register(IRegistrationContext $context): void
     {
-        // Register configuration
+        // Configuration
         $context->registerService(AppConfig::class, function($c) {
             return new AppConfig(
                 $c->get(IConfig::class),
@@ -76,29 +50,26 @@ class Application extends App implements IBootstrap
             );
         });
 
-        // Register database mappings
+        // Database mappers
         $context->registerService(JobMapper::class, function($c) {
             return new JobMapper(
-                $c->get(IDBConnection::class),
-                $c->get(Connection::class)
+                $c->get(IDBConnection::class)
             );
         });
 
         $context->registerService(AuditLogMapper::class, function($c) {
             return new AuditLogMapper(
-                $c->get(IDBConnection::class),
-                $c->get(Connection::class)
+                $c->get(IDBConnection::class)
             );
         });
 
         $context->registerService(DeviceMapper::class, function($c) {
             return new DeviceMapper(
-                $c->get(IDBConnection::class),
-                $c->get(Connection::class)
+                $c->get(IDBConnection::class)
             );
         });
 
-        // Register repositories
+        // Repositories
         $context->registerService(JobRepository::class, function($c) {
             return new JobRepository(
                 $c->get(JobMapper::class)
@@ -117,7 +88,7 @@ class Application extends App implements IBootstrap
             );
         });
 
-        // Register adapters
+        // Adapters
         $context->registerService(UserAdapter::class, function($c) {
             return new UserAdapter(
                 $c->get(IUserManager::class),
@@ -127,7 +98,8 @@ class Application extends App implements IBootstrap
 
         $context->registerService(TokenAdapter::class, function($c) {
             return new TokenAdapter(
-                $c->get(ISecureRandom::class)
+                $c->get(ISecureRandom::class),
+                $c->get(IDBConnection::class)
             );
         });
 
@@ -146,28 +118,41 @@ class Application extends App implements IBootstrap
             );
         });
 
-        // Register logger
+        // Logger
         $context->registerService(LoggerFactory::class, function($c) {
             return new LoggerFactory(self::APP_ID);
         });
 
-        $context->registerService(AppLogger::class, function($c) {
-            return $c->get(LoggerFactory::class)->getLogger();
-        });
-
-        // Register audit logger
+        // Audit
         $context->registerService(AuditLogger::class, function($c) {
             return new AuditLogger(
                 $c->get(AuditLogRepository::class),
-                $c->get(AppLogger::class)
+                $c->get(LoggerFactory::class)->getLogger()
             );
         });
 
-        // Register queue services
+        // Driver
+        $context->registerService(DriverFactory::class, function($c) {
+            return new DriverFactory(
+                $c->get(TokenAdapter::class),
+                $c->get(LoggerFactory::class)->getLogger()
+            );
+        });
+
+        // Queue
         $context->registerService(JobQueue::class, function($c) {
             return new JobQueue(
                 $c->get(JobRepository::class),
-                $c->get(AppLogger::class)
+                $c->get(LoggerFactory::class)->getLogger()
+            );
+        });
+
+        $context->registerService(QueueManager::class, function($c) {
+            return new QueueManager(
+                $c->get(JobQueue::class),
+                $c->get(JobProcessor::class),
+                $c->get(AppConfig::class),
+                $c->get(LoggerFactory::class)->getLogger()
             );
         });
 
@@ -175,41 +160,17 @@ class Application extends App implements IBootstrap
             return new JobProcessor(
                 $c->get(JobRepository::class),
                 $c->get(AuditLogger::class),
-                $c->get(AppLogger::class),
+                $c->get(LoggerFactory::class)->getLogger(),
                 $c->get(NextcloudAdapter::class)
             );
         });
 
-        // Register commands
-        $context->registerCommand(Command\OffboardUser::class);
-        $context->registerCommand(Command\DisableUsers::class);
-        $context->registerCommand(Command\DeleteTokens::class);
-        $context->registerCommand(Command\RemoteWipe::class);
-        $context->registerCommand(Command\ProcessQueue::class);
-        $context->registerCommand(Command\AuditLog::class);
-        $context->registerCommand(Command\CleanupAuditLogs::class);
-        $context->registerCommand(Command\ListDevices::class);
-
-        // Register background jobs
-        $context->registerBackgroundJob(\OCA\AdminOffboard\BackgroundJob\ProcessQueueJob::class);
-        $context->registerBackgroundJob(\OCA\AdminOffboard\BackgroundJob\CleanupAuditLogJob::class);
-
-        // Register settings
-        $context->registerAdminSetting(\OCA\AdminOffboard\Settings\AdminSettings::class);
-        $context->registerAdminSection(\OCA\AdminOffboard\Settings\AdminSection::class);
-
-        // Register activity providers
-        $context->registerActivityFilter(\OCA\AdminOffboard\Activity\Filter::class);
-        $context->registerActivitySetting(\OCA\AdminOffboard\Activity\Setting::class);
-        $context->registerActivityProvider(\OCA\AdminOffboard\Activity\Provider::class);
+        // Register OCC commands
+        $context->registerService(\OCA\AdminOffboard\Command\TestCommand::class);
     }
 
-    /**
-     * Boot the application
-     */
     public function boot(IBootContext $context): void
     {
-        // Initialize application
         $context->getAppContainer()->query(AppConfig::class)->init();
     }
 }
--- lib/Logger/LoggerFactory.php
+++ lib/Logger/LoggerFactory.php
@@ -2,78 +2,17 @@
 
 declare(strict_types=1);
 
-/**
- * @copyright Copyright (c) 2024 Metrat <disparam@gmail.com>
- *
- * @author Metrat <disparam@gmail.com>
- *
- * @license AGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace OCA\AdminOffboard\Logger;
 
-use OCP\ILogger;
-use OCP\Log\ILogFactory;
-use OCP\AppFramework\Services\Logger;
-
-/**
- * Logger factory for the application
- */
 class LoggerFactory
 {
-    private ?AppLogger $logger = null;
-
     public function __construct(
-        private string $appId,
-        private ILogFactory $logFactory,
-        private ILogger $loggerService
+        private string $appId
     ) {
     }
 
-    /**
-     * Get the application logger
-     */
     public function getLogger(): AppLogger
     {
-        if ($this->logger === null) {
-            $this->logger = new AppLogger(
-                $this->appId,
-                $this->logFactory,
-                $this->loggerService
-            );
-        }
-
-        return $this->logger;
-    }
-
-    /**
-     * Create a new logger instance with a specific context
-     */
-    public function createContextLogger(array $context): AppLogger
-    {
-        $logger = $this->getLogger();
-        return $logger->withContext($context);
-    }
-
-    /**
-     * Create a component logger
-     */
-    public function getComponentLogger(string $component): AppLogger
-    {
-        $logger = $this->getLogger();
-        return $logger->withContext(['component' => $component]);
+        return new AppLogger($this->appId);
     }
 }
