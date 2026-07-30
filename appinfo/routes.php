0de66f25e3a63fa632562de107e9b3103b6667c5
Metrat <disparam@gmail.com>
2026-07-30 12:10:16 +0300

    v0.1.5: Stable CLI & API release - Removed non-working Vue.js web interface - Added .gitignore and .env.example - Removed .htaccess, phpstan.neon, psalm.xml - 6 working OCC commands - 8 API endpoints - PHP 8.4 & Nextcloud 34 compatible

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,27 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * @copyright Copyright (c) 2024 Metrat <disparam@gmail.com>
- *
- * @author Metrat <disparam@gmail.com>
- *
- * @license AGPL-3.0-or-later
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 return [
     'routes' => [
         // API routes - version 1
@@ -64,27 +43,6 @@ return [
         [
             'name' => 'Api#getQueueStats',
             'url' => '/api/v1/queue/stats',
-            'verb' => 'GET',
-        ],
-        // Web UI routes
-        [
-            'name' => 'Page#index',
-            'url' => '/',
-            'verb' => 'GET',
-        ],
-        [
-            'name' => 'Page#dashboard',
-            'url' => '/dashboard',
-            'verb' => 'GET',
-        ],
-        [
-            'name' => 'Page#offboard',
-            'url' => '/offboard',
-            'verb' => 'GET',
-        ],
-        [
-            'name' => 'Page#settings',
-            'url' => '/settings',
             'verb' => 'GET',
         ],
     ],
